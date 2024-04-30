23b0634e6763989ddace46d50a6fd48c15b9c5ed
Trung Ma <trung.ma@iths.se>
2024-04-30 16:03:51 +0200

    Created GBPConvertion

--- Consumer/src/main/java/org/example/consumer/Main.java
+++ Consumer/src/main/java/org/example/consumer/Main.java
@@ -17,8 +17,8 @@ public class Main {
 
             double amountInUSDFromEUR = converter.revertUSD(USD);
 
-            System.out.println(USD + "$ = " + amountInEUR + converter.currencySymbol());
-            System.out.println("100.0" + converter.currencySymbol() + " = " + amountInUSDFromEUR + "$");
+            System.out.println(USD + "$ USD = " + amountInEUR + converter.currencySymbol());
+            System.out.println("100.0" + converter.currencySymbol() + " = " + amountInUSDFromEUR + "$ USD");
         }
     }
 }
--- Provider/src/main/java/org/example/provider/ConvertUSDToGBP.java
+++ Provider/src/main/java/org/example/provider/ConvertUSDToGBP.java
@@ -2,20 +2,20 @@ package org.example.provider;
 
 import org.example.service.CurrencyConverter;
 
-public class ConvertUSDToEUR implements CurrencyConverter {
+public class ConvertUSDToGBP implements CurrencyConverter {
 
-    private static final double USD_TO_EUR = 0.93;
-    private static final double EUR_TO_USD = 1.07;
-    private static final String symbol = "€ EUR";
+    private static final double USD_TO_GBP = 0.80;
+    private static final double GBP_TO_USD = 1.25;
+    private static final String symbol = "£ GBP";
 
     @Override
     public double convertUSD(double amount) {
-        return amount * USD_TO_EUR;
+        return amount * USD_TO_GBP;
     }
 
     @Override
     public double revertUSD(double amount) {
-        return amount * EUR_TO_USD;
+        return amount * GBP_TO_USD;
     }
 
     @Override
