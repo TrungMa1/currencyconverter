e7614c646d64127cab5db4babbb1f101c06d8b43
Trung Ma <trung.ma@iths.se>
2024-04-30 16:05:49 +0200

    Created SEKConversion

--- Provider/src/main/java/module-info.java
+++ Provider/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 import org.example.provider.ConvertUSDToEUR;
 import org.example.provider.ConvertUSDToGBP;
+import org.example.provider.ConvertUSDToSEK;
 import org.example.service.CurrencyConverter;
 
 module provider {
     requires org.example.service;
-    provides CurrencyConverter with ConvertUSDToEUR, ConvertUSDToGBP;
+    provides CurrencyConverter with ConvertUSDToEUR, ConvertUSDToGBP, ConvertUSDToSEK;
 }
--- Provider/src/main/java/org/example/provider/ConvertUSDToSEK.java
+++ Provider/src/main/java/org/example/provider/ConvertUSDToSEK.java
@@ -2,20 +2,20 @@ package org.example.provider;
 
 import org.example.service.CurrencyConverter;
 
-public class ConvertUSDToEUR implements CurrencyConverter {
+public class ConvertUSDToSEK implements CurrencyConverter {
 
-    private static final double USD_TO_EUR = 0.93;
-    private static final double EUR_TO_USD = 1.07;
-    private static final String symbol = "€ EUR";
+    private static final double USD_TO_SEK = 10.97;
+    private static final double SEK_TO_USD = 0.091;
+    private static final String symbol = "Kr SEK";
 
     @Override
     public double convertUSD(double amount) {
-        return amount * USD_TO_EUR;
+        return amount * USD_TO_SEK;
     }
 
     @Override
     public double revertUSD(double amount) {
-        return amount * EUR_TO_USD;
+        return amount * SEK_TO_USD;
     }
 
     @Override
